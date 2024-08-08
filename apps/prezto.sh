2c4ffd976368bb6cbf888878eff08d60207a67cf
Petr <petr@duck.com>
2024-08-08 23:07:32 +0300

    prezto adapted for mac also

--- apps/prezto.sh
+++ apps/prezto.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-if [[ "$ID" == "ubuntu" || "$ID" == "debian" ]]; then
-	sudo apt update
-	sudo apt install -y zsh git
-elif [ "$ID" = "fedora" ]; then
-	dnf check-update
-	sudo dnf install -y zsh git
-elif [ "$ID" = "opensuse" ]; then
-	sudo zypper refresh
-	sudo zypper install zsh git
-elif [ "$ID_LIKE" = "arch" ]; then
-	sudo pacman -Sy zsh git
+if [[ $(uname -o) == "Darwin" ]]; then
+	echo zsh is macOS default
+
+else
+
+	if [[ "$ID" == "ubuntu" || "$ID" == "debian" ]]; then
+		sudo apt update
+		sudo apt install -y zsh git
+	elif [ "$ID" = "fedora" ]; then
+		dnf check-update
+		sudo dnf install -y zsh git
+	elif [ "$ID" = "opensuse" ]; then
+		sudo zypper refresh
+		sudo zypper install zsh git
+	elif [ "$ID_LIKE" = "arch" ]; then
+		sudo pacman -Sy zsh git
+	fi
+
+	sudo sed -i "s|^\($USER.*\)/bin/bash|\1/bin/zsh|" /etc/passwd
 fi
 
 zsh <<'EOF'
@@ -20,5 +28,3 @@ for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
   ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
 done
 EOF
-
-sudo sed -i "s|^\($USER.*\)/bin/bash|\1/bin/zsh|" /etc/passwd
