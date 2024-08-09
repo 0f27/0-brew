d3cc41678eee7bad7f800481acc0598fdc09aa42
Petr <petr@duck.com>
2024-08-09 03:30:45 +0300

    emax enstallation if not exists added

--- apps/doom.sh
+++ apps/doom.sh
@@ -1,5 +1,32 @@
 #!/usr/bin/env sh
 
+if ! command -v emacs &>/dev/null; then
+    if [[ $(uname -o) == "Darwin" ]]; then
+        brew install emacs
+
+    else
+
+        . /etc/os-release
+
+        if [[ "$ID" == "ubuntu" || "$ID" == "debian" ]]; then
+            sudo apt update
+            sudo apt install -y emacs
+
+        elif [ "$ID" = "fedora" ]; then
+            dnf check-update
+            sudo dnf install -y emacs
+
+        elif [ "$ID" = "opensuse" ]; then
+            sudo zypper refresh
+            sudo zypper install emacs
+
+        elif [ "$ID_LIKE" = "arch" ]; then
+            sudo pacman -Sy --noconfirm emacs
+
+        fi
+    fi
+fi
+
 mkdir -p ~/.config
 rm -rf ~/.emacs ~/.emacs.d ~/.config/emacs
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
