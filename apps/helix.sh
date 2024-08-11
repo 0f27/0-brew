9702e84927ef4323d72fe277a8dd1ef42f28df0b
Petr <petr@duck.com>
2024-08-11 09:46:29 +0300

    install helix only if command not available

--- apps/helix.sh
+++ apps/helix.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
-if [[ $(uname -o) == "Darwin" ]]; then
-	brew install helix
+if ! command -v hx &>/dev/null; then
+  if [[ $(uname -o) == "Darwin" ]]; then
+  	brew install helix
 
-elif [[ $(uname -o) == "Android" ]]; then
-  apt update
-  apt install -y helix
-
-else
-
-  . /etc/os-release
-
-  if [ "$ID" == "ubuntu" ]; then
-
-    sudo add-apt-repository ppa:maveonair/helix-editor
-    sudo apt update
-    sudo apt install -y helix
-
-  elif [ "$ID" = "fedora" ]; then
-    dnf check-update
-    sudo dnf install -y helix
-
-  elif [ "$ID_LIKE" = "arch" ]; then
-    sudo pacman -Sy --noconfirm helix
-    sudo ln -s /usr/bin/helix /usr/bin/hx
+  elif [[ $(uname -o) == "Android" ]]; then
+    apt update
+    apt install -y helix
 
   else
-    echo $ID not suppotred
 
+    . /etc/os-release
+
+    if [ "$ID" == "ubuntu" ]; then
+
+      sudo add-apt-repository ppa:maveonair/helix-editor
+      sudo apt update
+      sudo apt install -y helix
+
+    elif [ "$ID" = "fedora" ]; then
+      dnf check-update
+      sudo dnf install -y helix
+
+    elif [ "$ID_LIKE" = "arch" ]; then
+      sudo pacman -Sy --noconfirm helix
+      sudo ln -s /usr/bin/helix /usr/bin/hx
+
+    else
+      echo $ID not suppotred
+
+    fi
   fi
-
 fi
 
 mkdir -p ~/.config/helix
