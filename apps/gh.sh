a3b8957bf5ab2ace8571877a24456edcf217b86c
Petr <petr@duck.com>
2024-08-11 09:51:59 +0300

    gh and lazygit to install only if not avaliable

--- apps/gh.sh
+++ apps/gh.sh
@@ -1,30 +1,31 @@
 #!/usr/bin/env bash
 
-if [[ $(uname -o) == "Darwin" ]]; then
-	brew install gh
+if ! command -v gh &>/dev/null; then
+	if [[ $(uname -o) == "Darwin" ]]; then
+		brew install gh
 
-elif [[ $(uname -o) == "Android" ]]; then
-  apt update
-  apt install -y gh
+	elif [[ $(uname -o) == "Android" ]]; then
+	  apt update
+	  apt install -y gh
 
-else
+	else
 
-	. /etc/os-release
+		. /etc/os-release
 
-	if [[ "$ID" == "ubuntu" || "$ID" == "debian" ]]; then
-		(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) &&
-			sudo mkdir -p -m 755 /etc/apt/keyrings &&
-			wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null &&
-			sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg &&
-			echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
-			sudo apt update &&
-			sudo apt install -y gh
-	elif [ "$ID" = "fedora" ]; then
-		sudo dnf install 'dnf-command(config-manager)'
-		sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
-		sudo dnf install -y gh
-	elif [ "$ID_LIKE" = "arch" ]; then
-	    sudo pacman -Sy github-cli
+		if [[ "$ID" == "ubuntu" || "$ID" == "debian" ]]; then
+			(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) &&
+				sudo mkdir -p -m 755 /etc/apt/keyrings &&
+				wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null &&
+				sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg &&
+				echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+				sudo apt update &&
+				sudo apt install -y gh
+		elif [ "$ID" = "fedora" ]; then
+			sudo dnf install 'dnf-command(config-manager)'
+			sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+			sudo dnf install -y gh
+		elif [ "$ID_LIKE" = "arch" ]; then
+		    sudo pacman -Sy github-cli
+		fi
 	fi
-
 fi
--- apps/lazygit.sh
+++ apps/lazygit.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
-. /etc/os-release
+if ! command -v lazygit &>/dev/null; then
+  if [[ $(uname -o) == "Darwin" ]]; then
+  	brew install lazygit
 
-if [ "$ID" = "fedora" ]; then
-	sudo dnf copr enable atim/lazygit -y
-	sudo dnf install -y lazygit
-elif [ "$ID_LIKE" = "arch" ]; then
-    sudo pacman -Sy lazygit
-else
-	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-	tar xf lazygit.tar.gz lazygit
-	sudo install lazygit /usr/local/bin
+  elif [[ $(uname -o) == "Android" ]]; then
+    apt update
+    apt install -y lazygit
+
+  else
+		. /etc/os-release
+
+	if [ "$ID" = "fedora" ]; then
+		sudo dnf copr enable atim/lazygit -y
+		sudo dnf install -y lazygit
+	elif [ "$ID_LIKE" = "arch" ]; then
+	    sudo pacman -Sy lazygit
+	else
+		LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+		curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+		tar xf lazygit.tar.gz lazygit
+		sudo install lazygit /usr/local/bin
+	fi
 fi
