850939d6066d7a26172def588efd4cee50c71cea
Petr <petr@duck.com>
2024-08-16 20:08:01 +0300

    function to de-serialize saved os info implemented

---
+++ os-info/os-check-analyse.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import json
+
+
+def get_os_info(info_file):
+    with open(info_file, "r") as f:
+        os_info = f.read()
+
+    os_info = "{" + os_info.split("{")[-1].replace("'", '"')
+    return json.loads(os_info)
+
+
+if __name__ == "__main__":
+    from pprint import pprint
+
+    pprint(get_os_info("os-check-thumbleweed.txt"))
