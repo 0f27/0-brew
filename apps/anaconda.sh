8ff1a3b06cab9c40ff06acf498bfcf5104243f16
Petr <petr@duck.com>
2024-08-16 12:47:22 +0300

    anaconda - installation for macos added, version checking for GNU Linux refactored

--- apps/anaconda.sh
+++ apps/anaconda.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env sh
 
 if [[ $(uname -o) == "Darwin" ]]; then
-	echo macOS version currently not implemented
+	brew install --cask anaconda
 
 elif [[ $(uname -o) == "Android" ]]; then
 	echo termux version currently not implemented
 
 else
 	if ! test -f "$HOME/.anaconda3/bin/activate"; then
+		VERSION="$(curl -sL https://formulae.brew.sh/cask/anaconda | grep 'Current version' | cut -d'>' -f3 | cut -d'<' -f1)"
 		if command -v aria2c &>/dev/null; then
-			aria2c https://repo.anaconda.com/archive/Anaconda3-$(curl -sL https://formulae.brew.sh/cask/anaconda | grep "Current version" | cut -d'>' -f3 | cut -d'<' -f1)-Linux-x86_64.sh && bash Anaconda3* -b -p ~/.anaconda3 && rm Anaconda3*
+			aria2c https://repo.anaconda.com/archive/Anaconda3-$VERSION-Linux-x86_64.sh && bash Anaconda3* -b -p ~/.anaconda3 && rm Anaconda3*
 		elif command -v wget &>/dev/null; then
-			wget https://repo.anaconda.com/archive/Anaconda3-$(curl -sL https://formulae.brew.sh/cask/anaconda | grep "Current version" | cut -d'>' -f3 | cut -d'<' -f1)-Linux-x86_64.sh && bash Anaconda3* -b -p ~/.anaconda3 && rm Anaconda3*
+			wget https://repo.anaconda.com/archive/Anaconda3-$VERSION-Linux-x86_64.sh && bash Anaconda3* -b -p ~/.anaconda3 && rm Anaconda3*
 		fi
 	fi
 fi
