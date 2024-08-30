01478fe3e9e9f21553d0e11be2b111dab8f09e16
Petr <petr@duck.com>
2024-08-30 14:41:43 +0300

    paths created and file reformatted

--- apps/lazyvim.sh
+++ apps/lazyvim.sh
@@ -19,14 +19,13 @@ if [ ! -f ~/.config/nvim/init.lua ]; then
   ln -s ~/.config/lazyvim ~/.config/nvim
 fi
 
-(
-  NVIM_APPNAME=lazyvim
+NVIM_APPNAME=lazyvim
 
-  git clone https://github.com/LazyVim/starter ~/.config/lazyvim
-  rm -rf ~/.config/lazyvim/.git
+git clone https://github.com/LazyVim/starter ~/.config/lazyvim
+rm -rf ~/.config/lazyvim/.git
 
-  mkdir -p ~/.config/lazyvim/lua/plugins
-  cat <<'EOF' >~/.config/lazyvim/lua/plugins/nvim-notify.lua
+mkdir -p ~/.config/lazyvim/lua/plugins
+cat <<'EOF' >~/.config/lazyvim/lua/plugins/nvim-notify.lua
 return {
     "rcarriga/nvim-notify",
     opts = {
@@ -37,7 +36,8 @@ return {
 }
 EOF
 
-  cat <<'EOF' >~/.config/lazyvim/lua/config/keymaps.lua
+mkdir -p ~/.config/lazyvim/lua/config
+cat <<'EOF' >~/.config/lazyvim/lua/config/keymaps.lua
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
@@ -53,8 +53,8 @@ map("n", "<leader>fs", "<cmd>w<cr>", { noremap = true, desc = "Save buffer" })
 map("n", "<leader>cd", "<cmd>cd %:p:h<cr>", { noremap = true, desc = "cd to this file" })
 map("n", "<leader>bb", "<cmd>Telescope buffers<CR>", { desc = "Find buffers" })
 EOF
-)
 
+mkdir -p ~/.config/lazyvim/lua/plugins
 cat <<'EOF' >~/.config/lazyvim/lua/plugins/core.lua
 return {
   {
