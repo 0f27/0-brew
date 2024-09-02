09ba005c33ec5accfb1d5c3d0fea347880e68152
Petr <petr@duck.com>
2024-09-02 03:02:21 +0300

    doom modified to use custom directoru

--- apps/doom.sh
+++ apps/doom.sh
@@ -1,42 +1,52 @@
 #!/usr/bin/env bash
 
 if ! command -v emacs &>/dev/null; then
-    if [[ $(uname -o) == "Darwin" ]]; then
-        brew install emacs
+  if [[ $(uname -o) == "Darwin" ]]; then
+    brew install emacs
 
-    elif [[ $(uname -o) == "Android" ]]; then
-      apt update
-      apt install -y emacs
+  elif [[ $(uname -o) == "Android" ]]; then
+    apt update
+    apt install -y emacs
 
-    else
+  else
 
-        . /etc/os-release
+    . /etc/os-release
 
-        if [[ "$ID" == "fedora" && "$VARIANT_ID" != "silverblue" && "$VARIANT_ID" != "kinoite" ]]; then
-            dnf check-update
-            sudo dnf install -y emacs
+    if [[ "$ID" == "fedora" && "$VARIANT_ID" != "silverblue" && "$VARIANT_ID" != "kinoite" ]]; then
+      dnf check-update
+      sudo dnf install -y emacs
 
-        elif [[ "$VARIANT_ID" == "silverblue" || "$VARIANT_ID" == "kinoite" ]]; then
-          sudo rpm-ostree install --apply-live -y emacs
+    elif [[ "$VARIANT_ID" == "silverblue" || "$VARIANT_ID" == "kinoite" ]]; then
+      sudo rpm-ostree install --apply-live -y emacs
 
-        elif [ "$ID_LIKE" = "opensuse suse" ]; then
-            sudo zypper refresh
-            sudo zypper --non-interactive --no-confirm install emacs
+    elif [ "$ID_LIKE" = "opensuse suse" ]; then
+      sudo zypper refresh
+      sudo zypper --non-interactive --no-confirm install emacs
 
-        elif [[ "$ID" == "debian" || "$ID_LIKE" == "debian" || "$ID_LIKE" == "ubuntu debian" ]]; then
-            sudo apt update
-            sudo apt install -y emacs
+    elif [[ "$ID" == "debian" || "$ID_LIKE" == "debian" || "$ID_LIKE" == "ubuntu debian" ]]; then
+      sudo apt update
+      sudo apt install -y emacs
 
-        elif [ "$ID_LIKE" = "arch" ]; then
-            sudo pacman -Sy --noconfirm emacs
+    elif [ "$ID_LIKE" = "arch" ]; then
+      sudo pacman -Sy --noconfirm emacs
 
-        fi
     fi
+  fi
 fi
 
-if [ ! -f $HOME/.config/emacs/bin/doom ]; then
-    mkdir -p $HOME/.config
-    rm -rf $HOME/.emacs $HOME/.emacs.d $HOME/.config/emacs
-    git clone --depth 1 https://github.com/doomemacs/doomemacs $HOME/.config/emacs
-    $HOME/.config/emacs/bin/doom install --env -!
+EMACS_VERSION="$(emacs --version | head -n 1 | cut -d' ' -f 3)"
+EMACS_INIT_DIRECTORY="$HOME/.opt/doom"
+
+if [ ! -d $EMACS_INIT_DIRECTORY ]; then
+  mkdir -p $EMACS_INIT_DIRECTORY/.config
+  # rm -rf $HOME/.emacs $HOME/.emacs.d $HOME/.config/emacs
+  git clone --depth 1 https://github.com/doomemacs/doomemacs $EMACS_INIT_DIRECTORY/.config/emacs
+  $EMACS_INIT_DIRECTORY/.config/emacs/bin/doom install --env -!
 fi
+
+cat <<'EOF' >~/.local/bin/doom
+#!/usr/bin/env bash
+
+emacs --init-directory=$HOME/.opt/doom/.config/emacs $@
+EOF
+chmod +x ~/.local/bin/doom
