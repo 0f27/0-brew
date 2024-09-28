47828a27cf9c1eabbf8a90bc8f31c36c87351102
Petr <petr@duck.com>
2024-09-28 06:14:49 +0300

    doom to install to default emacs config dir only

--- apps/doom.sh
+++ apps/doom.sh
@@ -34,24 +34,8 @@ if ! command -v emacs &>/dev/null; then
   fi
 fi
 
-EMACS_VERSION="$(emacs --version | head -n 1 | cut -d' ' -f 3)"
-
-if [ -d $HOME/.config/emacs ] && [ -f $HOME/.emacs ] && [ -d $HOME/.emacs ] && [ -d $HOME/.emacs.d ]; then
-  EMACS_INIT_DIRECTORY="$HOME/.opt/doom"
-
-  mkdir -p ~/.local/bin
-  cat <<'EOF' >~/.local/bin/doom
-#!/usr/bin/env bash
-
-emacs --init-directory=$HOME/.opt/doom/.config/emacs $@
-EOF
-  chmod +x ~/.local/bin/doom
-
-else
-  EMACS_INIT_DIRECTORY=$HOME
-fi
-
-if [ ! -d $EMACS_INIT_DIRECTORY ]; then
+EMACS_INIT_DIRECTORY="$HOME"
+if [ ! -d $EMACS_INIT_DIRECTORY/.config/emacs ]; then
   mkdir -p $EMACS_INIT_DIRECTORY/.config
   # rm -rf $HOME/.emacs $HOME/.emacs.d $HOME/.config/emacs
   git clone --depth 1 https://github.com/doomemacs/doomemacs $EMACS_INIT_DIRECTORY/.config/emacs
