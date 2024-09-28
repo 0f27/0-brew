0424873336f70e29ec61817d31387ace621b95ab
Petr <petr@duck.com>
2024-09-28 09:04:05 +0300

    more complete deps

--- apps/doom.sh
+++ apps/doom.sh
@@ -2,33 +2,37 @@
 
 if ! command -v emacs &>/dev/null; then
   if command -v brew &>/dev/null; then
-    brew install emacs
+    brew install emacs git ripgrep fd
 
   elif [[ $(uname -o) == "Android" ]]; then
     apt update
-    apt install -y emacs
+    apt install -y emacs git ripgrep fd
 
   else
 
     . /etc/os-release
 
     if command -v pacman &>/dev/null; then
-      sudo pacman -Sy --noconfirm emacs
+      sudo pacman -Sy --noconfirm emacs git ripgrep fd
 
     elif command -v rpm-ostree &>/dev/null; then
-      sudo rpm-ostree install --apply-live -y emacs
+      sudo rpm-ostree install --apply-live -y emacs git ripgrep fd-find
 
     elif command -v zypper &>/dev/null; then
       sudo zypper refresh
-      sudo zypper --non-interactive --no-confirm install emacs
+      sudo zypper --non-interactive --no-confirm install emacs git ripgrep fd-find
 
     elif command -v dnf &>/dev/null; then
       dnf check-update
-      sudo dnf install -y emacs
+      sudo dnf install -y emacs git ripgrep fd-find
+
+    elif command -v snap &>/dev/null; then
+      sudo snap instal emacs --classic
+      sudo snap instal ripgrep --classic
 
     elif command -v apt &>/dev/null; then
       sudo apt update
-      sudo apt install -y emacs
+      sudo apt install -y emacs ripgrep git fd-find
 
     fi
   fi
