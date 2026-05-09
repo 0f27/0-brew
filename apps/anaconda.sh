cbe4b6df2e872a115ae944e28a4f9fe34c5d2aef
Petr <petr@duck.com>
2026-05-09 12:03:50 +0700

    anaconda -- command order fixed

--- apps/anaconda.sh
+++ apps/anaconda.sh
@@ -7,7 +7,6 @@ elif [[ $(uname -o) == "Android" ]]; then
 	echo termux version currently not implemented
 
 else
-  PACKAGE=$(curl -sL https://repo.anaconda.com/archive/ | grep Anaconda3 | grep Linux | grep "$(uname -m)" | head -n 1 | cut -d\" -f2)
 	if ! test -f "$HOME/.anaconda3/bin/activate"; then
 		if command -v aria2c &>/dev/null; then
 			TOOL=aria2c
@@ -16,6 +15,7 @@ else
     elif command -v curl &>/dev/null; then
       TOOL="curl -LO"
     fi
+    PACKAGE=$(curl -sL https://repo.anaconda.com/archive/ | grep Anaconda3 | grep Linux | grep "$(uname -m)" | head -n 1 | cut -d\" -f2)
     $TOOL https://repo.anaconda.com/archive/$PACKAGE && bash $PACKAGE -b -p ~/.anaconda3 && rm $PACKAGE
 	fi
 fi
